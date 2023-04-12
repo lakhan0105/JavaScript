d74c6b7825902769fbac55e05e440dd89e0460ef
Lakhan Kumar <lakhan0105@gmail.com>
2023-04-12 12:04:18 +0530

    Added preloader

--- 02_js_john_smilga_course/06_section6_DOM_Projects/09_video/script.js
+++ 02_js_john_smilga_course/06_section6_DOM_Projects/09_video/script.js
@@ -1,7 +1,10 @@
 // Selecting the elements
 const videoEl = document.querySelector(".video-container");
 const btn = document.querySelector(".checkbox-input");
+const preLoaderEl = document.querySelector(".preloader");
 let count = 0;
+
+// Event Listener to play and pause
 btn.addEventListener("click", function () {
   count++;
   if (count === 1) {
@@ -10,4 +13,10 @@ btn.addEventListener("click", function () {
     count = 0;
     videoEl.play();
   }
+});
+
+// load event
+// add hide class when the page loads and show other contents
+window.addEventListener("load", function () {
+  preLoaderEl.classList.add("hide-preloader");
 });
